e2552ae82c515cc0e248feada42196a78b0d5395
jekichuu <74702647+jekichuu@users.noreply.github.com>
2020-11-20 13:28:05 +0700

    Added basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    //public Rigidbody2D rb;
+    public Transform movePoint;
+    //Vector2 movement;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+    void Update()
+    {
+        // Input Free Move
+        //movement.x = Input.GetAxisRaw("Horizontal");
+        //movement.y = Input.GetAxisRaw("Vertical");
+
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
+
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
