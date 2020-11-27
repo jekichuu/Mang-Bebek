88c544704a543a019c763817152580bcbe83028f
esterinac <esterinachristina@gmail.com>
2020-11-27 14:01:00 +0700

    Added Enemies Movement

--- Assets/Scripts/EnemiesBehaviour.cs
+++ Assets/Scripts/EnemiesBehaviour.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class EnemiesBehaviour : MonoBehaviour
 {
-
+    public float speed;
+    public bool MoveRight;
 
     // Start is called before the first frame update
-    void Start()
+/*    void Start()
     {
         
     }
-
+*/
     // Update is called once per frame
     void Update()
     {
-        
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2(1, 1);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0,0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.CompareTag("turn"))
+        {
+
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
     }
 }
